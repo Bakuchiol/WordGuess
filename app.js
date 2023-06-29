dd9c768264ade03f01400cdaab220817a8f8bebe
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-28 20:14:11 -0500

    switch player function

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-/******************************************** Variables */
+/******************************************** DOM */
 let guessButton = document.querySelector('.guessButton');
+let input = document.querySelector('input');
+
+
+/******************************************** VARIABLES */
+let words = []; // store words entered here (words to guess)
+let currentPlayer = false; // initial player one
+
+
 
 // event listener for guess button
 const guessWord = () => {
+    // console.log('button workssss');
+    // console.log(input.value);
+    words.unshift(input.value);
+    // console.log(words);
 
+}
+
+// swap players
+const playerSwitch = () => {
+    if(currentPlayer === false){
+        currentPlayer = true;
+    }
 }

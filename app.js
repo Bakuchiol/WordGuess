0f6aaf0027f8fa3fe0f75a06c85b4a4dc0e1fd15
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-28 20:40:14 -0500

    update status text

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 let guessButton = document.querySelector('.guessButton');
 let input = document.querySelector('input');
 let playerNumber = document.querySelector('span');
+let currentStatus = document.querySelector('.currentStatus')
 
 
 /******************************************** VARIABLES */
 let words = []; // store words entered here (words to guess)
 let currentPlayer = 1; // initial player one
+let standing = false;
 
 
 
@@ -14,9 +16,17 @@ let currentPlayer = 1; // initial player one
 const guessWord = () => {
     // console.log('button workssss');
     // console.log(input.value);
-    words.unshift(input.value);
-    // console.log(words);
-    playerSwitch()
+    // define game status/standing - players storing words
+    if(standing === false){
+        words.unshift(input.value);
+        playerSwitch();
+        // tell if second player turn
+        if(words.length === 2){
+            currentStatus.textContent = `Guess your opponent's spell!`
+        }
+    }
+
+    console.log(words);
 
 }
 
